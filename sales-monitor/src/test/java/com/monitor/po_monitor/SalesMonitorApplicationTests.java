b92d7c20e7a605d45641eb29ba61218bdeabe1c6
Rahul <rahul_kr_saxena@yahoo.in>
2026-03-19 12:15:12 +0530

    MCP Server

--- sales-monitor/src/test/java/com/monitor/po_monitor/SalesMonitorApplicationTests.java
+++ sales-monitor/src/test/java/com/monitor/po_monitor/SalesMonitorApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PoMonitorApplicationTests {
+class SalesMonitorApplicationTests {
 
 	@Test
 	void contextLoads() {
